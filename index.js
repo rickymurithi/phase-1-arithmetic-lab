122fcae0278e0e5e30288bbdafde58b3414a6c5a
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-08-20 18:49:10 +0300

    add file

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1=31, num2=2;
+const multiply=(num1*num2);
+
+const random=Math.floor(Math.random()*10);
+
+const num3=12, num4=3;
+const mod=(num3/num4);
+
+const max=Math.max(10,15,20)
